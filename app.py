2f00b1f615a1acc0a4ec61faae18459c6c332d7b
Teja Surya <tejasurya@alumni.iitm.ac.in>
2025-04-03 01:06:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ def flashcards():
     return render_template('flashcards.html', flashcard=flashcards[next_index])
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=False, host='0.0.0.0')
